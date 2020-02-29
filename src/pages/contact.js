7462887f78b1fce2051848dc660eda5dd125d7eb
franklam2008 <franklam2008@gmail.com>
2020-02-28 23:55:47 -0500

    add contact form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,21 +1,151 @@
-import React from "react"
-import { Link } from "gatsby"
+import React, { useState, useRef } from "react"
+import axios from "axios"
+
 import { useSiteMetadata } from "../hooks/use-site-metadata"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-
+import wave from "../images/wave/topWave.svg"
+import styled from "styled-components"
+import { Container, Checkbox, Form, Button, Icon } from "semantic-ui-react"
+const ContactCon = styled.div`
+  .ContactNav {
+    margin: 20px auto;
+    justify-content: center;
+    text-align: center;
+    a {
+      svg {
+        color: var(--darkerSaved);
+        font-size: 1.5rem;
+        margin: 0 10px;
+        &:hover {
+          color: var(--mainColorSaved);
+        }
+      }
+    }
+  }
+`
+const MadeWith = styled.p`
+  margin: 20px;
+  text-align: center;
+  .heart {
+    color: var(--specialColorSaved);
+    padding-top: 2px;
+  }
+`
 export default () => {
   const { title } = useSiteMetadata()
+  const [formMsg, setFormMsg] = useState("")
+  const [checkbox, setCheckbox] = useState(false)
+  const emailInput = useRef()
+  const nameInput = useRef()
+  const phoneInput = useRef()
+  const messageInput = useRef()
   return (
     <Layout>
       <SEO title="Contact" />
-      <h1>Contact Me</h1>
-      <h1>welcome to {title}</h1>
-      <p>
-        Let's talk! I would love to hear your project idea, I'll get the beer.
-      </p>
-      <Link to="/">Go back to the homepage</Link>
+      <ContactCon className="page">
+        <div className="contactTopCon topCon"></div>
+        <img className="TopWave" src={wave} alt="wave" />
+        <Container>
+          <h2 className="pageTitle">Contact Me</h2>
+          {title}
+          <Form className="contactForm" onSubmit={handleForm}>
+            <p>
+              Let's talk! I would love to hear your project idea, I'll get the
+              beer.
+            </p>
+            {formMsg !== "Submission Successful" ? (
+              <>
+                <Form.Field>
+                  <input
+                    className="inputSaved"
+                    type="text"
+                    ref={nameInput}
+                    placeholder="Name"
+                    required
+                  />
+                </Form.Field>
+                <Form.Field>
+                  <input
+                    className="inputSaved"
+                    type="email"
+                    ref={emailInput}
+                    placeholder="Enter email"
+                    required
+                  />
+                </Form.Field>
+                <Form.Field>
+                  <input
+                    className="inputSaved"
+                    type="tel"
+                    ref={phoneInput}
+                    placeholder="Phone Number"
+                    required
+                  />
+                </Form.Field>
+
+                <Form.Field>
+                  <input
+                    className="inputSaved"
+                    type="text"
+                    ref={messageInput}
+                    placeholder="Message"
+                    required
+                  />
+                </Form.Field>
+
+                <Form.Field>
+                  <Checkbox
+                    checked={checkbox}
+                    onChange={() => setCheckbox(!checkbox)}
+                    label="Send me a SMS &amp; Email"
+                  />
+                </Form.Field>
+                <p>
+                  We'll never share your information with anyone else saving it
+                  in Airtable and send you a SMS and Email via Twilio and
+                  SendGrid API.
+                </p>
+                <Button type="submit">Submit</Button>
+              </>
+            ) : (
+              <div>Thank you!</div>
+            )}
+            <span style={{ marginLeft: 10 }}>{formMsg}</span>
+          </Form>
+
+          <MadeWith>
+            Made with <Icon name="heart" /> in Orlando, Florida
+          </MadeWith>
+        </Container>
+      </ContactCon>
     </Layout>
   )
+  function handleForm(e, value) {
+    e.preventDefault()
+    const input = {
+      name: nameInput.current.value,
+      email: emailInput.current.value,
+      phone: phoneInput.current.value,
+      message: messageInput.current.value,
+      checkbox: checkbox,
+    }
+
+    sendForm(input)
+  }
+  function sendForm(input) {
+    console.log(input)
+    axios
+      // .post("http://localhost:4000/submit", input)
+      .post("https://secure-peak-92770.herokuapp.com/submit", input)
+      .then(res => {
+        console.log(res)
+        setFormMsg(res.data)
+      })
+      .catch(err => {
+        console.log(err)
+        setFormMsg("Connected Error")
+      })
+  }
 }
