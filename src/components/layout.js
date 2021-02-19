f8889880048555dac30dcbda25ff3b42788c4859
Frank Lam <franklam2008@gmail.com>
2021-02-18 19:23:17 -0500

    testing

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,8 +1,8 @@
 import React, { useState } from "react"
 import PropTypes from "prop-types"
-//pages
+// pages
 import Header from "./header"
-//css
+// css
 import styled from "styled-components"
 import "./css/layout.css"
 import "./css/app.css"
