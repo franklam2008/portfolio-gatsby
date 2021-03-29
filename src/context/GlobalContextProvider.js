785ab314ee6d35698ff5f0111e2d2193a68fe483
Frank Lam <franklam2008@gmail.com>
2021-03-28 22:37:20 -0400

    test store

---
+++ src/context/GlobalContextProvider.js
@@ -0,0 +1,47 @@
+import React from "react"
+
+export const GlobalStateContext = React.createContext()
+export const GlobalDispatchContext = React.createContext()
+
+const initialState = {
+  pageChanging: false,
+  currentPage: "/",
+}
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "PAGE_CHANGING": {
+      const isPageDifferent = state.currentPage !== action.payload
+      const nextPage = isPageDifferent ? action.payload : state.currentPage
+      return {
+        ...state,
+        // pageChanging: state.pageChanging ? false : true,
+        pageChanging: isPageDifferent,
+
+        currentPage: nextPage,
+      }
+    }
+    case "PAGE_CHANGED": {
+      return {
+        ...state,
+        pageChanging: false,
+      }
+    }
+    //default
+    default:
+      return state
+  }
+}
+
+const GlobalContextProvider = ({ children }) => {
+  const [state, dispatch] = React.useReducer(reducer, initialState)
+  return (
+    <GlobalStateContext.Provider value={state}>
+      <GlobalDispatchContext.Provider value={dispatch}>
+        {children}
+      </GlobalDispatchContext.Provider>
+    </GlobalStateContext.Provider>
+  )
+}
+
+export default GlobalContextProvider
