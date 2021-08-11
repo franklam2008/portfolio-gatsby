473d66c988bfb26e715c2250b705a6ec6f2346af
Frank Lam <41360206+franklam2008@users.noreply.github.com>
2021-08-11 18:36:51 -0400

    test comment

--- src/components/header.js
+++ src/components/header.js
@@ -89,6 +89,7 @@ const Header = () => {
     <header>
       <TopNavbarCSS>
         {/* Left Nav */}
+        {/* test comment */}
         <div className="topNavLeft">
           <button onClick={() => navigationOnClick("/", dispatch)}>
             <MainIcon draggable="false" src={frankIcon} alt="main Icon" />
